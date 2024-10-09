039146e2c69f18a540627c6b0c4dda2b9906e306
AbrarAlenezi <abrar.alenezi@hotmail.com>
2024-10-09 18:13:17 +0300

    this is the first commit

---
+++ AFSQL/AFSQL.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OSIsoft.AF.Asset;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Data;
+using System.Data.SqlClient;
+using OSIsoft.AF.Time;
+using OSIsoft.AF.Data;
+
+namespace AFSDK_AvocetDR
+{
+    /******************************************************************************
+     * A custom data reference for the PI AF server that retrieves the timestamps and
+     * values from a specified SQL table. 
+     *****************************************************************************/
+    [Serializable]
+    [Guid("42B2AC6D-8A11-49DD-BD92-DC1F67CCBCC5")]
+    [Description("AvocetDR10.1; Get values from slb Avocet SQLName, DBName, TableNam, FieldColumn, TimestampColumn")]
+    public class AFSQL : AFDataReference
+    {
+        // Private fields storing configuration of data reference
+        private string _tableName = String.Empty;
+        private string _dbName = String.Empty;
+        private string _sqlName = String.Empty;
+        private string _fieldcolumn = String.Empty;
+        private string _timestampcolumn=String.Empty;
+
+        // Public property for name of the SQL table
+        public string TableName
+        {
+            get
+            {
+                return _tableName;
+            }
+            set
+            {
+                if (_tableName != value)
+                {
+                    _tableName = value;
+                    SaveConfigChanges();
+                }
+            }
+        }
+
+        // Public property for name of the SQL database
+        public string DBName
+        {
+            get
+            {
+                return _dbName;
+            }
+            set
+            {
+                if (_dbName != value)
+                {
+                    _dbName = value;
+                    SaveConfigChanges();
+                }
+            }
+        }
+
+        // Public property for name of the SQL instance
+        public string SQLName
+        {
+            get
+            {
+                return _sqlName;
+            }
+            set
+            {
+                if (_sqlName != value)
+                {
+                    _sqlName = value;
+                    SaveConfigChanges();
+                }
+            }
+        }
+
+        // Public property for name of the SQL table
+        public string FieldColumn
+        {
+            get
+            {
+                return _fieldcolumn;
+            }
+            set
+            {
+                if (_fieldcolumn != value)
+                {
+                    _fieldcolumn = value;
+                    SaveConfigChanges();
+                }
+            }
+        }
+
+        // Public property for name of the SQL table
+        public string TimestampColumn
+        {
+            get
+            {
+                return _timestampcolumn;
+            }
+            set
+            {
+                if (_timestampcolumn != value)
+                {
+                    _timestampcolumn = value;
+                    SaveConfigChanges();
+                }
+            }
+        }
+
+        // Get or set the config string for the SQL data reference
+        public override string ConfigString
+        {
+            get
+            {
+                return String.Format("{0};{1};{2};{3};{4}", SQLName, DBName, TableName, FieldColumn, TimestampColumn);
+            }
+            set
+            {
+                if (value != null)
+                {
+                    string[] configSplit = value.Split(';');
+                    SQLName = configSplit[0];
+                    DBName = configSplit[1];
+                    TableName = configSplit[2];
+                    FieldColumn = configSplit[3];
+                    TimestampColumn = configSplit[4];
+                    SaveConfigChanges();
+                }
+            }
+        }
+
+        // Return latest value if timeContext is null, otherwise return latest value before a specific time
+        public override AFValue GetValue(object context, object timeContext, AFAttributeList inputAttributes, AFValues inputValues)
+        {
+            AFValue currentVal = new AFValue();
+            DateTime time;
+            // Get the TimeZone ID to handle Day Saving Time
+             TimeZoneInfo localTZID = TimeZoneInfo.Local;
+            //TimeZoneInfo localTZ = TimeZoneInfo.FindSystemTimeZoneById(localTZID);
+
+            if (timeContext != null)
+            {
+                time = ((AFTime)timeContext).LocalTime;
+            }
+            else
+            {
+                time = AFTime.Now;
+                
+            }
+            using (SqlDataReader reader = SQLHelper.GetSQLData(SQLName, DBName, TableName, DateTime.MinValue, time, FieldColumn, TimestampColumn))
+            {
+                if (reader.Read())
+                {
+                    DateTime queryOfSQLTimeStamp = DateTime.Parse(reader[0].ToString());
+                    queryOfSQLTimeStamp = DateTime.SpecifyKind(queryOfSQLTimeStamp, DateTimeKind.Local);
+                    if (localTZID.IsInvalidTime(queryOfSQLTimeStamp))
+                    {                        
+                        currentVal.Timestamp = queryOfSQLTimeStamp.AddHours(1);
+                        //currentVal.Value = -100;
+
+                    }
+                    else if(localTZID.IsAmbiguousTime(queryOfSQLTimeStamp)) 
+                    {
+                        currentVal.Timestamp = queryOfSQLTimeStamp.AddHours(-1);
+                        //currentVal.Value = -200;
+                    }
+                    else
+                    {
+                        currentVal.Timestamp = queryOfSQLTimeStamp ;
+                        currentVal.Value = reader[1];
+
+                    }
+
+                    
+
+                }
+            }
+
+            return currentVal;
+        }
+
+        // Return all values (converted to AFValues) over a specific time interval
+        public override AFValues GetValues(object context, AFTimeRange timeRange, int numberOfValues, AFAttributeList inputAttributes, AFValues[] inputValues)
+        {
+            AFValues values = new AFValues();
+            // Get the TimeZone ID to handle Day Saving Time
+            TimeZoneInfo localTZID = TimeZoneInfo.Local;
+            //TimeZoneInfo localTZ = TimeZoneInfo.FindSystemTimeZoneById(localTZID);
+            DateTime startTime = DateTime.SpecifyKind(timeRange.StartTime, DateTimeKind.Local);
+            DateTime endTime = DateTime.SpecifyKind(timeRange.EndTime, DateTimeKind.Local);
+            using (SqlDataReader reader = SQLHelper.GetSQLData(SQLName, DBName, TableName, startTime, endTime, FieldColumn, TimestampColumn))
+            {
+                while (reader.Read())
+                {
+                    AFValue newVal = new AFValue();
+                    DateTime queryOfSQLTimeStamp = DateTime.Parse(reader[0].ToString());
+                    queryOfSQLTimeStamp=DateTime.SpecifyKind(queryOfSQLTimeStamp, DateTimeKind.Local);
+                    if (localTZID.IsInvalidTime(queryOfSQLTimeStamp))
+                    {                      
+                        newVal.Timestamp = queryOfSQLTimeStamp.AddHours(1);                        
+                    }
+                    else if(localTZID.IsAmbiguousTime(queryOfSQLTimeStamp))
+                    {
+                        newVal.Timestamp = queryOfSQLTimeStamp.AddHours(-1);
+                    }
+                    else 
+                    {
+                        newVal.Timestamp = queryOfSQLTimeStamp;                           
+                    }
+
+                    newVal.Value = reader[1];
+                    values.Add(newVal);
+                }
+            }
+            return values;
+        }
+
+        // Return an AFEventSource object for this custom data reference
+        public static object CreateDataPipe()
+        {
+            EventSource pipe = new EventSource();
+            return pipe;
+        }
+
+        public override AFDataReferenceMethod SupportedMethods
+        {
+            get
+            {
+                return AFDataReferenceMethod.GetValue | AFDataReferenceMethod.GetValues;
+            }
+        }
+
+        public override AFDataMethods SupportedDataMethods
+        {
+            get
+            {
+                return AFDataMethods.DataPipe;
+            }
+        }
+
+        public override AFDataReferenceContext SupportedContexts
+        {
+            get
+            {
+                return AFDataReferenceContext.Time;
+            }
+        }
+
+
+
+    }
+}
---
+++ AFSQL/SQLHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AFSDK_AvocetDR
+{
+    /*****************************************************************************************
+     * SQL Helper class to get data from the specified SQL Server, SQL Database and SQL Table. 
+     * Given a start and end time, the static GetSQLData method will return a SQLDataReader
+     * object that contains all values between the time interval in ascending order.
+     * Given the end time only, only the most current value will be returned. 
+     *****************************************************************************************/
+    class SQLHelper
+    {
+        public static SqlDataReader GetSQLData(string sqlServer, string sqlDb, string sqlTable, DateTime startTime, DateTime endTime, string sqlcolumn, string sqlTimestamp)
+        {
+            // Construct connection string to SQL Server based on input parameters for SQL server name and database name.
+            string connectString = String.Format("server={0}; database={1}; Integrated Security=SSPI; Connection Timeout=10", sqlServer, sqlDb);
+            SqlConnection sqlConnection = new SqlConnection(connectString);
+
+            // Construct SQL query
+            string query;
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = sqlConnection;
+
+                // SQL query for the most recent values before the end time
+                if (startTime == DateTime.MinValue)
+                {
+                    query = String.Format("SELECT TOP 1 {3}, {2} FROM {0}.{1} WHERE {3} <= @time ORDER BY {3} DESC", sqlDb, sqlTable, sqlcolumn, sqlTimestamp);
+                    SqlParameter sqlTime = cmd.Parameters.Add(new SqlParameter("time", System.Data.SqlDbType.DateTime2));
+                    sqlTime.Value = endTime;
+                    cmd.CommandText = query;
+                }
+
+                // SQL query for all values over a specified time range
+                else
+                {
+                    query = String.Format("SELECT {3}, {2} FROM {0}.{1} WHERE {3} >= @startTime AND {3} <= @endTime ORDER BY {3} ASC", sqlDb, sqlTable, sqlcolumn, sqlTimestamp);
+                    SqlParameter sqlStartTime = cmd.Parameters.Add(new SqlParameter("startTime", System.Data.SqlDbType.DateTime2));
+                    SqlParameter sqlEndTime = cmd.Parameters.Add(new SqlParameter("endTime", System.Data.SqlDbType.DateTime2));
+                    sqlStartTime.Value = startTime;
+                    sqlEndTime.Value = endTime;
+                    cmd.CommandText = query;
+                }
+
+                /* Open SQL connection and return the SqlDataReader object. Use CommandBehavior.CloseConnection to ensure that the 
+                 * SQL connection is closed when the SqlDataReader object is closed. */
+                sqlConnection.Open();
+                SqlDataReader sqlReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                return sqlReader;
+            }
+        }
+    }
+}
